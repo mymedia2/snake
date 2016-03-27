bce6d209a4d0c014ed3d0e06ad76fc6eaed21c9d
Guriev Nicholas <guriev-ns@ya.ru>
2016-03-27 08:22:26 +0300

    Добавлен класс Assertion

--- utils.hpp
+++ utils.hpp
@@ -1,7 +1,10 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef UTILS_HPP
+#define UTILS_HPP
 
 #include <cstddef>
+#include <exception>
+#include <stdexcept>
+#include <string>
 
 namespace utils {
 
@@ -29,6 +32,28 @@ struct Coordinates
  * на игровом поле. */
 typedef Coordinates<std::size_t> Point;
 
+/* Класс ложного инварианта времени выполнения */
+class Assertion
+	: public std::logic_error
+	, public std::bad_exception
+{
+public:
+	/* Создаёт исключение с информацией о провале */
+	inline Assertion(std::string filename, std::size_t lineno, std::string expression);
+};
+
+/* Макрос, вызывающий служебную функцию проверки инварианта. Выражение
+ * вычисляется только в случае если не объявлен макрос NDEBUG */
+#ifndef NDEBUG
+#define assert(expr) ( ::utils::_assert((expr), __FILE__, __LINE__, #expr) )
+#else
+#define assert(expr) ( static_cast<void>(false) )
+#endif
+
+/* Служебная функция. Не предназначена для самостоятельного вызова из
+ * клиентского кода */
+inline void _assert(bool, std::string, std::size_t, std::string);
+
 }	// namespace utils
 
 template <typename T>
@@ -44,4 +69,16 @@ inline utils::Coordinates<T>::Coordinates(T x, T y)
 {
 }
 
-#endif	// UTILS_H
+inline utils::Assertion::Assertion(std::string filename, std::size_t lineno, std::string expression)
+	: std::logic_error("Assertion '" + expression + "' at " + filename + ":" + std::to_string(lineno) + " failed")
+{
+}
+
+inline void utils::_assert(bool invariant, std::string filename, std::size_t lineno, std::string expression)
+{
+	if (!invariant) {
+		throw Assertion(filename, lineno, expression);
+	}
+}
+
+#endif	// UTILS_HPP
